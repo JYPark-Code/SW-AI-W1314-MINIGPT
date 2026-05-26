31aaae4a1228d4ba4c1b4e78bb8b90142ed6eb81
jypark <toboy93@gmail.com>
2026-05-26 19:14:55 +0900

    feat: create_dataloader 구현
    - GPTDataset을 DataLoader로 감싸 batch 단위로 제공
    - batch_size, shuffle, drop_last, num_workers 인자 전달
    
    test_dataloader_batch_shape 통과

--- src/dataset.py
+++ src/dataset.py
@@ -55,4 +55,13 @@ def create_dataloader(
     num_workers: int = 0,
 ) -> DataLoader:
     """TODO: GPTDataset을 만들고 torch.utils.data.DataLoader로 감싸 반환합니다."""
-    raise NotImplementedError("create_dataloader를 구현하세요.")
+
+    dataset = GPTDataset(token_ids, context_length, stride)
+    loader = DataLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=shuffle,
+        drop_last=drop_last,
+        num_workers=num_workers,
+    )
+    return loader
